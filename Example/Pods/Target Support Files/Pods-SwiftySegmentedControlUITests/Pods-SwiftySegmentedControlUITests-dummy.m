2b54a330c68269e529fddc0af5da500aa5079330
PlayApple <liuyanghui@LiuYanghuis-Mac.local>
2017-01-10 12:00:43 +0800

    rename example

--- Example/Pods/Target Support Files/Pods-SwiftySegmentedControlUITests/Pods-SwiftySegmentedControlUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftySegmentedControlUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftySegmentedControlUITests
-@end
--- Example/Pods/Target Support Files/SwiftySegmentedControl/SwiftySegmentedControl-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftySegmentedControl : NSObject
-@end
-@implementation PodsDummy_SwiftySegmentedControl
-@end
