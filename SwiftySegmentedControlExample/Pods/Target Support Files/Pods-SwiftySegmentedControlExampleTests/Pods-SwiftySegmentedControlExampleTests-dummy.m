77685df3930e523269305d015b5c47a29fee66e8
PlayApple <liuyanghui@LiuYanghuis-Mac.local>
2017-01-10 12:00:56 +0800

    rename example

---
+++ SwiftySegmentedControlExample/Pods/Target Support Files/Pods-SwiftySegmentedControlExampleTests/Pods-SwiftySegmentedControlExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftySegmentedControlExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftySegmentedControlExampleTests
+@end
---
+++ SwiftySegmentedControlExample/SwiftySegmentedControlExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftySegmentedControlExample
+//
+//  Created by LiuYanghui on 2017/1/10.
+//  Copyright © 2017年 Yanghui.Liu. All rights reserved.
+//
+
+import UIKit
+import SwiftySegmentedControl
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
