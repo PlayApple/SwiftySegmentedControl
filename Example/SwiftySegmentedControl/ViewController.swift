a6563c83a1be625a51bbac72a347a713ee3bd152
PlayApple <liuyanghui@LiuYanghuis-Mac.local>
2017-01-10 11:44:50 +0800

    first commit

--- Example/SwiftySegmentedControl/ViewController.swift
+++ Example/SwiftySegmentedControl/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import SwiftySegmentedControl
 
 class ViewController: UIViewController {
-
+    
+//    @IBOutlet weak var segmentedControl1: SwiftySegmentedControl!
+//    @IBOutlet weak var segmentedControl2: SwiftySegmentedControl!
+//    @IBOutlet weak var segmentedControl3: SwiftySegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        let a = SwiftySegmentedControl()
     }
 
     override func didReceiveMemoryWarning() {
